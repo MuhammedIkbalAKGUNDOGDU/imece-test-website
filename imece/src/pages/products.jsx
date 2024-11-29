4045d780d9d7496b183cd12e01d56967efd457c3
MuhammedIkbalAKGUNDOGDU <muhammedikbalcmp@gmail.com>
2024-11-29 17:19:33 +0300

    products and filter version 1 finished responsive design 70% finished

--- imece/src/pages/products.jsx
+++ imece/src/pages/products.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 import Header from "../components/GenerealUse/Header";
+import "../styles/products.css";
+import GrupAlimTekilAlim from "../components/landingPage/GrupAlimTekilAlim";
+import Filter from "../components/GenerealUse/filter";
+import ItemCard from "../components/GenerealUse/itemCard2";
 const products = () => {
   return (
-    <div className="prodcuts-body">
+    <div className="products-body">
       <Header />
+      <div className="grupalim-products">
+        {" "}
+        <GrupAlimTekilAlim />
+      </div>
+      <div className="products-products">
+        <Filter />
+        <div className="products-products-cards">
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+        </div>
+      </div>
     </div>
   );
 };

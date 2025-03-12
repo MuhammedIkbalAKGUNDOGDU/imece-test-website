35eb1afe3f20c1a14648ee03f6151d0b4d80bc12
MuhammedIkbalAKGUNDOGDU <muhammedikbalcmp@gmail.com>
2025-03-12 14:50:40 +0300

    .

--- imece/src/components/GenerealUse/ItemGrid.jsx
+++ imece/src/components/GenerealUse/ItemGrid.jsx
@@ -4,7 +4,6 @@ import ItemCard3 from "./ItemCard3";
 const ItemGrid = ({ items }) => {
   return (
     <div className="w-full px-4 sm:px-6 lg:px-6">
-
       {/* Kartların üst üste binmesini engelleyen düzenleme */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 place-items-center">
         {items.map((item) => (

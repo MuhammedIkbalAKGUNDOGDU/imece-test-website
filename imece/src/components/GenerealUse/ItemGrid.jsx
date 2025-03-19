e3eb19a69f5d9a6e752a92bb352a79ae413bd124
MuhammedIkbalAKGUNDOGDU <muhammedikbalcmp@gmail.com>
2025-03-20 01:12:05 +0300

    itemgrid fix

--- imece/src/components/GenerealUse/ItemGrid.jsx
+++ imece/src/components/GenerealUse/ItemGrid.jsx
@@ -5,14 +5,11 @@ import ItemCard4 from "./ItemCard4";
 const ItemGrid = ({ items, cardType = "card3" }) => {
   const CardComponent = cardType === "card4" ? ItemCard4 : ItemCard3;
   return (
-    <div className="w-full px-4 sm:px-6 lg:px-6">
-      {/* Kartların üst üste binmesini engelleyen düzenleme */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 place-items-center">
+    <div className="w-full">
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
         {items.map((item) => (
-          <div className="flex justify-center" key={item.urun_id || item.id}>
-            <div className="w-full max-w-[140px] sm:max-w-[220px] md:max-w-[260px]">
-              <CardComponent data={item} />
-            </div>
+          <div className="flex justify-center" key={item.id}>
+            <CardComponent data={item} />
           </div>
         ))}
       </div>

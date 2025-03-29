1638b730b5b6e790306e73fd24d58ee1a16793ce
MuhammedIkbalAKGUNDOGDU <muhammedikbalcmp@gmail.com>
2025-03-29 17:30:25 +0300

    favoriler page added

--- imece/src/pages/favoriler.jsx
+++ imece/src/pages/favoriler.jsx
@@ -1,13 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Header from "../components/GenerealUse/Header";
-const favoriler = () => {
+import ItemGrid from "../components/GenerealUse/ItemGrid";
+import LoadingSpinner from "../components/GenerealUse/loadingSpinner";
+
+const Favoriler = () => {
+  const [products, setProducts] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+  const [userId, setUserId] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const apiKey =
+    "WNjZXMttoxNzM5Mzc3MDM3LCJpYXQiOUvKrIq06hpJl_1PenWgeKZw_7FMvL65DixY";
+  const accessToken = localStorage.getItem("accessToken");
+
+  const headers = {
+    "X-API-Key": apiKey,
+    Authorization: `Bearer ${accessToken}`,
+    "Content-Type": "application/json",
+  };
+
+  const fetchFavorites = async () => {
+    try {
+      const userRes = await axios.get(
+        "https://imecehub.com/api/users/kullanicilar/me/",
+        { headers }
+      );
+      const id = userRes.data.id;
+      setUserId(id);
+
+      const fullUserRes = await axios.get(
+        `https://imecehub.com/api/users/kullanicilar/${id}/`,
+        { headers }
+      );
+      const userFavorites = fullUserRes.data.favori_urunler || [];
+      setFavorites(userFavorites);
+
+      const productsRes = await axios.get(
+        "https://imecehub.com/api/products/urunler/",
+        { headers }
+      );
+      const allProducts = productsRes.data;
+
+      console.log("favoriler : ", userFavorites, "others :", allProducts);
+      const favoriteProducts = allProducts.filter((product) =>
+        userFavorites.includes(product.urun_id)
+      );
+      setProducts(favoriteProducts);
+    } catch (err) {
+      console.error("Veri çekme hatası:", err);
+      setError("Veriler alınırken bir hata oluştu.");
+      setProducts(
+        mockProducts.filter((product) => favorites.includes(product.id))
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchFavorites();
+  }, []);
+
+  if (isLoading) return <LoadingSpinner />;
+  if (error) return <div className="text-red-600">{error}</div>;
+
   return (
-    <div>
-      <div className="mx-[4%] md:mx-[8%] mb-8">
-        <Header />
-      </div>{" "}
+    <div className="products-body">
+      <Header />
+      <div className="w-full max-w-[1400px] mx-auto px-4 md:mb-0 mb-28 mt-20">
+        {products.length > 0 ? (
+          <ItemGrid items={products} cardType="card4" favorites={favorites} />
+        ) : (
+          <p>Favori ürün bulunamadı</p>
+        )}
+      </div>
     </div>
   );
 };
 
-export default favoriler;
+export default Favoriler;
